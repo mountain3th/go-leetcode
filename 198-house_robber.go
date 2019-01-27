fcebe037226ef1aea1d5f98461ebde6acd322289
shengliu <shengliu@sohu-inc.com>
2019-01-27 12:26:56 +0800

    198-house_robber.go

--- 198-house_robber.go
+++ 198-house_robber.go
@@ -2,11 +2,31 @@ package main
 
 import "fmt"
 
-func rob(nums []int) int {
-	fmt.Println(len(nums))
-	return 1
+func max(p... int) int {
+    var x int = 0
+    for _, n := range p {
+        if n >= x {
+            x = n
+        }
+    }
+    return x
 }
 
-func main1() {
-	fmt.Println(rob([]int{1, 2}))
+func rob(nums []int) int {
+    dp := make([]int, len(nums), len(nums))
+    for i, n := range nums {
+        if i >= 3 {
+            dp[i] = max(dp[i-1], dp[i-2]+n, dp[i-3]+n)
+        } else if i >= 2 {
+            dp[i] = max(dp[i-1], dp[i-2]+n)
+        } else if i >= 1{
+            dp[i] = max(dp[i-1], n)
+        } else {
+            dp[i] = n
+        }
+    }
+    if len(dp) > 0 {
+        return dp[len(nums)-1]    
+    }
+    return 0
 }
